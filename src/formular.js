a8637a0029855843315786d4da53c9188639d1a1
Marius <marius1890@yahoo.com>
2024-05-01 21:02:40 +0300

    updates (obiect elemente_importante)

--- src/formular.js
+++ src/formular.js
@@ -3,13 +3,19 @@
 const IMPORTANTE = ["numele", "prenumele", "strada", "numarul", "localitate", "jud"]; // cele marcate din html cu *
 
 
-var contor = 0; // In functie de valoare, va afisa un buton de incarcare date din inputuri, pt afisare valoarea trebuie sa fie == 5 de la functia vericare camp
-var adevarat = null;
-let elem = document; // -> vezi iterarea pt eventuri
-var var_bool = true;
 
-function test() {
+let elem = document; // -> vezi iterarea pt eventuri
+
+let elemente_importante = {
+    // Daca toate elementele importante sunt adevarate va afisa un element html de tip button
+    "numele": false,
+    "prenumele": false,
+    "strada": false,
+    "numarul": false,
+    "localitate": false,
+    "jud": false,
 }
+
 
 function verificaCamp(camp)
 /* Provine din functia handler, cu scopul de a verifica conform cerintelor
@@ -19,101 +25,121 @@ fiecare camp (din toate elementele de tip input)
     const element = document.getElementById(camp);
     //console.log(element.value.trim());
     if (camp == "numele"){
-        if (element.value.trim().length < 3){
-            alert(`Campul ${camp.charAt(0).toUpperCase() + camp.slice(1)} trebuie sa contina minim trei litere`);
-            var_bool = false;
-            element.value = "";
-            document.getElementById("blocul").focus();
-            //return var_bool;
-        }
         for (var i = 0; i < element.value.trim().length; i++){
             if (!isNaN(element.value.trim()[i]) || !element.value.trim()[i].match(/[a-zA-Z]/)){
                 alert(`Campul ${camp.charAt(0).toUpperCase() + camp.slice(1)} nu este permis sa contina numere sau simboluri`);
-                var_bool = false;
                 element.value = "";
                 document.getElementById("blocul").focus();
+                elemente_importante[camp] = false;
                 break;
-                //return var_bool;
             }
+            else{
+                elemente_importante[camp] = true;
+            }
+            }
+        if (element.value.trim().length < 3){
+            alert(`Campul ${camp.charAt(0).toUpperCase() + camp.slice(1)} trebuie sa contina minim trei litere`);
+            element.value = "";
+            document.getElementById("blocul").focus();
+            elemente_importante[camp] = false;
         }
-        
-    }
+        else{
+            elemente_importante[camp] = true;
+        }
+        }
     else if (camp == "prenumele"){
-        if (element.value.trim().length < 3){
-            alert(`Campul ${camp.charAt(0).toUpperCase() + camp.slice(1)} trebuie sa contina minim trei litere`);
-            var_bool = false;
-            element.value = "";
-            document.getElementById("blocul").focus();
-            //return var_bool;
-        }
         for (var i = 0; i < element.value.trim().length; i++){
             if (!isNaN(element.value.trim()[i]) || !element.value.trim()[i].match(/[a-zA-Z]/)){
                 alert(`Campul ${camp.charAt(0).toUpperCase() + camp.slice(1)} nu este permis sa contina numere sau simboluri`);
-                var_bool = false;
                 element.value = "";
                 document.getElementById("blocul").focus();
+                elemente_importante[camp] = false;
                 break;
-                //return var_bool;
+            }
+            else{
+                elemente_importante[camp] = true;
             }
         }
+        if (element.value.trim().length < 3){
+            alert(`Campul ${camp.charAt(0).toUpperCase() + camp.slice(1)} trebuie sa contina minim trei litere`);
+            element.value = "";
+            document.getElementById("blocul").focus();
+            elemente_importante[camp] = false;
+        }
+        else{
+            elemente_importante[camp] = true;
+        }
+    
     }
     else if (camp == "strada"){
-        if (element.value.trim().length < 3){
-            alert(`Campul ${camp.charAt(0).toUpperCase() + camp.slice(1)} trebuie sa contina minim trei litere`);
-            var_bool = false;
-            element.value = "";
-            document.getElementById("blocul").focus();
-            //return var_bool;
-            }
         for (i = 0; i < element.value.trim().length; i++){
             if (!isNaN(element.value.trim()[i])){
                 alert(`Campul ${camp.charAt(0).toUpperCase() + camp.slice(1)} nu este permis sa contina cifre`);
-                var_bool = false;
                 element.value = "";
                 document.getElementById("blocul").focus();
+                elemente_importante[camp] = false;  
                 break;
-                //return var_bool;
             }
-        
+            else{
+                elemente_importante[camp] = true;
+            }
+        }
+        if (element.value.trim().length < 3){
+            alert(`Campul ${camp.charAt(0).toUpperCase() + camp.slice(1)} trebuie sa contina minim trei litere`);
+            element.value = "";
+            document.getElementById("blocul").focus();
+            elemente_importante[camp] = false;
+        }
+        else{
+            elemente_importante[camp] = true; 
         }
     }
     else if (camp == "localitate"){
         for (var i = 0; i < element.value.trim().length; i++){
             if (!isNaN(element.value.trim()[i])){
                 alert(`Campul ${camp.charAt(0).toUpperCase() + camp.slice(1)} nu este permis sa contina cifre`);
-                var_bool = false;
                 element.value = "";
                 document.getElementById("blocul").focus();
+                elemente_importante[camp] = false;
                 break;
-                //return var_bool;
+            }
+            else{
+                elemente_importante[camp] = true;
             }
     
         }
         if (element.value.trim().length < 3){
             alert(`Campul ${camp.charAt(0).toUpperCase() + camp.slice(1)} trebuie sa contina minim trei litere`);
-            var_bool = false;
             element.value = "";
             document.getElementById("blocul").focus();
-            //return var_bool;
+            elemente_importante[camp] = false;
+        }
+        else{
+            elemente_importante[camp] = true;
         }
         
     }
     else if (camp == "numarul"){
-        if (element.value.trim().length < 1){
-            alert(`Campul ${camp.charAt(0).toUpperCase() + camp.slice(1)} trebuie sa contina cel putin o cifra`);
-            var_bool = false;
-            element.value = "";
-            document.getElementById("blocul").focus();
-        }
         for (var i = 0; i <= element.value.trim().length; i++){
             if (!isNaN(element.value.trim()[i])){
+                elemente_importante[camp] = true;
                 break;
             }
             else if (i == (element.value.trim().length - 1)){
                 alert(`Campul ${camp.charAt(0).toUpperCase() + camp.slice(1)} trebuie sa contina cel putin o cifra`);
                 element.value = "";
                 document.getElementById("blocul").focus();
+                elemente_importante[camp] = false;
             }            
+        }
+        if (element.value.trim().length < 1){
+            alert(`Campul ${camp.charAt(0).toUpperCase() + camp.slice(1)} trebuie sa contina cel putin o cifra`);
+            element.value = "";
+            document.getElementById("blocul").focus();
+            elemente_importante[camp] = false;
+        }
+        else{
+            elemente_importante[camp] = true;
         }
 
     }
@@ -121,7 +147,18 @@ fiecare camp (din toate elementele de tip input)
         if (element.value.trim() == ""){
             alert(`Te rog selecteaza un judet`);
             document.getElementById("blocul").focus();
+            elemente_importante[camp] = false;
         }
+        else{
+            elemente_importante[camp] = true;
+        }
+    }
+    console.clear();
+    for (let elem1 in elemente_importante){
+        console.log(elem1 + ": " + elemente_importante[elem1]);
+    }
+    if (elemente_importante["numele"] == true && elemente_importante["prenumele"] == true && elemente_importante["strada"] == true && elemente_importante["numarul"] == true && elemente_importante["localitate"] == true && elemente_importante["jud"] == true){
+
     }
 }
 
